bbd353e44df83196b8b40dbb4caa40a95613ab8e
Ahmed Ali <129698762+TheAhmedJoy@users.noreply.github.com>
2026-09-08 15:04:01 -0700

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,58 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        top = 0
+        bottom = len(matrix) - 1
+
+        while top <= bottom:
+            middle_row = top + ((bottom - top) // 2)
+
+            if matrix[middle_row][-1] < target:
+                top = middle_row + 1
+            elif matrix[middle_row][0] > target:
+                bottom = middle_row - 1
+            else:
+                break
+        
+        if not (top <= bottom):
+            return False
+
+        left = 0
+        right = len(matrix[0]) - 1 
+        middle_row = top + ((bottom - top) // 2)
+
+        while left <= right:
+            middle_col = left + ((right - left) // 2)
+
+            if matrix[middle_row][middle_col] < target:
+                left = middle_col + 1
+            elif matrix[middle_row][middle_col] > target:
+                right = middle_col - 1
+            else:
+                return True
+        
+        return False
+
+        # while top <= bottom:
+        #     middle_row = top + ((bottom - top) // 2)
+
+        #     if matrix[middle_row][0] > target:
+        #         bottom = middle_row - 1
+        #     elif matrix[middle_row][0] < target and matrix[middle_row][-1] > target:
+        #                 left = 0
+        #                 right = len(matrix[middle_row])
+
+        #                 while left <= right:
+        #                     middle_col = left + ((right - left) // 2)
+
+        #                     if matrix[middle_row][middle_col] > target:
+        #                         right = middle_col - 1
+        #                     elif matrix[middle_row][middle_col]  < target:
+        #                         left = middle_col - 1
+        #                     else:
+        #                         return True
+                        
+        #                 return false
+        #     else:
+        #         return True
+        
+        # return False
